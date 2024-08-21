e8cbcdcacdfec8cf6ce27ab8865e3dc6147ba9b4
dgsw09 <rlaehdgus08@dgsw09ui-MacBookAir.local>
2024-08-21 19:46:01 +0900

    컨트롤러

---
+++ src/main/java/modi/modurang/member/MemberEntity.java
@@ -0,0 +1,22 @@
+package modi.modurang.member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Table(name = "member")
+@Entity
+@Getter
+@Setter
+@ToString
+public class MemberEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private int studentnumber;
+}
